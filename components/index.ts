7b45625db1db685d54585adf972b85c6df91ff15
Hoang <uthoang2002@gmail.com>
2024-04-05 06:32:09 +0700

    update

---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import CarCard  from './CarCard';
+import SearchBar  from './SearchBar';
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import CardDetails from './CardDetails';
+
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CardDetails
+}
